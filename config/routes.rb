e8ce807d557eb538c40ed09c46b8305dbdc3df20
kouichi shinkokura <stndard809@gmail.com>
2023-01-30 17:48:34 +0900

    production3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  post '/admin/users/new', to: 'admin/users#create'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  post '/admin/users/new', to: 'admin/users#create'
   namespace :admin do
     resources :users
   end
