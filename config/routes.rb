10b75a093a9ece6b550d7dbe699da329acb3df71
kouichi shinkokura <stndard809@gmail.com>
2023-01-30 14:38:35 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'tasks#index'
   post '/admin/users/new', to: 'admin/users#create'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
